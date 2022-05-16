634afa8d193064ba721d15b43ce8ed0e88c7dc7b
tyinfinity <infinityhd101@gmail.com>
2022-05-16 23:56:50 +0200

    update Lab2 Task2

---
+++ Lab2/Task2/Fraction.cs
@@ -0,0 +1,190 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Fraction
+    {
+        private long denominator;
+
+        public long Numerator { get; set; }
+        public long Denominator
+        {
+            get => denominator;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                denominator = value;
+
+                if (value < 0)
+                {
+                    Numerator *= -1;
+                    denominator *= -1;
+                }
+            }
+        }
+
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Fraction(long numerator, long denominator)
+        {
+            if (denominator < 0)
+            {
+                numerator *= -1;
+                denominator *= -1;
+            }
+
+            if (numerator == 0)
+            {
+                denominator = 1;
+            }
+
+            if (denominator == 0)
+            {
+                throw new Exception("Нельзя делить на 0.");
+            }
+
+            long nod = Nod(numerator, denominator);
+            numerator /= nod;
+            denominator /= nod;
+
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Fraction f)
+            {
+                return false;
+            }
+
+            return (Numerator == f.Numerator) && (Denominator == f.Denominator);
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(CalculateNumerator(Nok(fraction1, fraction2), fraction1) + CalculateNumerator(Nok(fraction1, fraction2), fraction2), Nok(fraction1, fraction2));
+        }
+
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(CalculateNumerator(Nok(fraction1, fraction2), fraction1) - CalculateNumerator(Nok(fraction1, fraction2), fraction2), Nok(fraction1, fraction2));
+        }
+
+        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.Numerator * fraction2.Numerator, fraction1.Denominator * fraction2.Denominator);
+        }
+
+        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1 * new Fraction(fraction2.Denominator, fraction2.Numerator);
+        }
+
+        public static bool operator ==(Fraction fraction1, Fraction fraction2)
+        {
+            if (CalculateNumerator(Nok(fraction1, fraction2), fraction1) == CalculateNumerator(Nok(fraction1, fraction2), fraction2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Fraction fraction1, Fraction fraction2)
+        {
+            if (CalculateNumerator(Nok(fraction1, fraction2), fraction1) != CalculateNumerator(Nok(fraction1, fraction2), fraction2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator >(Fraction fraction1, Fraction fraction2)
+        {
+            if (CalculateNumerator(Nok(fraction1, fraction2), fraction1) > CalculateNumerator(Nok(fraction1, fraction2), fraction2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator <(Fraction fraction1, Fraction fraction2)
+        {
+            if (CalculateNumerator(Nok(fraction1, fraction2), fraction1) < CalculateNumerator(Nok(fraction1, fraction2), fraction2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public static bool operator >=(Fraction fraction1, Fraction fraction2)
+        {
+            if (CalculateNumerator(Nok(fraction1, fraction2), fraction1) >= CalculateNumerator(Nok(fraction1, fraction2), fraction2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator <=(Fraction fraction1, Fraction fraction2)
+        {
+            if (CalculateNumerator(Nok(fraction1, fraction2), fraction1) <= CalculateNumerator(Nok(fraction1, fraction2), fraction2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static long Nod(long a, long b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a = a % b;
+                }
+                else
+                {
+                    b = b % a;
+                }
+            }
+
+            return a + b;
+        }
+
+        private static long Nok(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1.Denominator * fraction2.Denominator / Nod(fraction1.Denominator, fraction2.Denominator);
+        }
+
+        private static long CalculateNumerator(long nok, Fraction fraction)
+        {
+            return fraction.Numerator * (nok / fraction.Denominator);
+        }
+    }
+}
--- Lab2/Task2/Program.cs
+++ Lab2/Task2/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Task2
 {
-    internal class Program
+    internal class Program : Fraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fraction fraction1 = new Fraction(1, 4);
+            Fraction fraction2 = new Fraction(1, 3);
+            
+            var multiply = fraction1 * fraction2;
+            var divide = fraction1 / fraction2;
+            var sum = fraction1 + fraction2;
+            var sub = fraction1 - fraction2;
+            var more = fraction1 > fraction2;
+            var less = fraction1 < fraction2;
+            var equal = fraction1 == fraction2;
+
+            Console.WriteLine($"Результат умножения: {multiply}. Результат деления: {divide}. \n" +
+                              $"Результат сложения: {sum}. Результат вычитания: {sub}. \n" +
+                              $"Первая дробь больше второй: {more}. Вторая дробь больше первой: {less}. \n" +
+                              $"Дроби равны: {equal} и {fraction1.Equals(fraction2)}.");
         }
     }
 }
